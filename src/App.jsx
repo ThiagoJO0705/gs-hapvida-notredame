31bdb2004cb82992bfa45825d9d748795e7d6806
ThiagoJO0705 <rm551624@fiap.com.br>
2023-11-17 13:05:55 -0300

    começando estilização login

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 
+import { Outlet } from 'react-router-dom'
 import './App.css'
+
 
 function App() {
   return (
     <>
-        <h1>Home</h1>
+        <h1>Menu</h1>
+
+        <Outlet/>
     </>
   )
 }
