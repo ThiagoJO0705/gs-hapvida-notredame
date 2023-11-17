eb99499dfb986adf52e868f4729565f9546ea6f0
ThiagoJO0705 <rm551624@fiap.com.br>
2023-11-17 12:09:10 -0300

    adicionando rota de erro

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+
+//BLOCO DAS ROTAS
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Erro404 from './Routes/Erro404.jsx';
+
+
+//BLOCO DAS ROTAS
+const router = createBrowserRouter([
+  {path:"/",element: <App/>,errorElement:<Erro404/>,
+   children:[
+    
+   ]
+ },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
