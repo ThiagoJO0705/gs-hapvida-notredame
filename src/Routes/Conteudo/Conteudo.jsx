e168777912e8af6b7ae979c9731c5a7b6c6910b3
ThiagoJO0705 <rm551624@fiap.com.br>
2023-11-18 18:51:18 -0300

    fazendo a ação

--- src/Routes/Conteudo/Conteudo.jsx
+++ src/Routes/Conteudo/Conteudo.jsx
@@ -1,7 +1,8 @@
-import Carrossel from "../../Components/Carrossel/Carrossel.jsx"
-import Solucao from "../../Components/Solucao/Solucao.jsx"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./Conteudo.scss"
+import Carrossel from "../../Components/Carrossel/Carrossel.jsx"
+import Solucao from "../../Components/Solucao/Solucao.jsx"
+import Acao from "../../Components/Acao/Acao.jsx"
 
 function Conteudo() {
 
@@ -10,47 +11,8 @@ function Conteudo() {
     <>
         <Carrossel />
         <Solucao />
-        {/* <section className='acao'>
-            <div>
-                    <h1>O Que Ele Fará ?</h1>
-                <div>
-    <div className="card">
-    <div className="content">
-        <div className="back">
-        <div className="back-content">
-        
-            imagem
-        
-            <h3>Monitoramento Contínuo</h3>
-            <strong>Hover Me</strong>
-        </div>
-        </div>
-        <div className="front">
-      
-        <div className="img">
-            <div className="circle">
-            </div>
-            <div className="circle" id="right">
-            </div>
-            <div className="circle" id="bottom">
-            </div>
-        </div>
-
-        <div className="front-content">
-            <div className="description">
-            <div className="title">
-                <p className="title">
-                <strong>Sensores integrados acompanham sinais vitais e atividade física em tempo real, fornecendo dados cruciais para entender a saúde do usuário</strong>
-                </p>
-            </div>
-            </div>
-        </div>
-        </div>
-    </div>
-    </div>
-                </div>
-            </div>
-        </section> */}
+        <Acao />
+ 
     </>
 
   );
