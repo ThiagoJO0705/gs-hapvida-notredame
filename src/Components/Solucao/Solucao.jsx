9c34c767d3ea7d0ad3f79963824ab856ac566f38
ThiagoJO0705 <rm551624@fiap.com.br>
2023-11-18 17:05:50 -0300

    transformando Solucao em componente

---
+++ src/Components/Solucao/Solucao.jsx
@@ -0,0 +1,15 @@
+import "./Solucao.scss"
+
+export default function Solucao() {
+
+    return (
+        <section id='solucao' className='solucao'>
+        <div className='container-solucao'>
+            <div className='texto-solucao'>
+                <h1>HealthTrackr</h1>
+                <p>A nossa solução é uma plataforma de saúde digital integrada, centrada em um smartwatch e seus aplicativos associados. Este dispositivo inovador utiliza tecnologias avançadas para oferecer um monitoramento contínuo da saúde, proporcionando uma abordagem proativa para a prevenção de doenças e promoção do bem-estar. Com sensores específicos e algoritmos sofisticados, a solução visa revolucionar a maneira como as pessoas cuidam da sua saúde, oferecendo informações precisas e personalizadas em tempo real.</p>
+            </div>
+        </div>
+    </section>
+    )
+}
--- src/Routes/Conteudo/Conteudo.jsx
+++ src/Routes/Conteudo/Conteudo.jsx
@@ -1,4 +1,5 @@
 import Carrossel from "../../Components/Carrossel/Carrossel.jsx"
+import Solucao from "../../Components/Solucao/Solucao.jsx"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./Conteudo.scss"
 
@@ -8,16 +9,7 @@ function Conteudo() {
   return (
     <>
         <Carrossel />
-
-        <section id='solucao' className='solucao'>
-            <div className='container-solucao'>
-                <div className='texto-solucao'>
-                    <h1>HealthTrackr</h1>
-                    <p>A nossa solução é uma plataforma de saúde digital integrada, centrada em um smartwatch e seus aplicativos associados. Este dispositivo inovador utiliza tecnologias avançadas para oferecer um monitoramento contínuo da saúde, proporcionando uma abordagem proativa para a prevenção de doenças e promoção do bem-estar. Com sensores específicos e algoritmos sofisticados, a solução visa revolucionar a maneira como as pessoas cuidam da sua saúde, oferecendo informações precisas e personalizadas em tempo real.</p>
-                </div>
-            </div>
-        </section>
-
+        <Solucao />
         {/* <section className='acao'>
             <div>
                     <h1>O Que Ele Fará ?</h1>
