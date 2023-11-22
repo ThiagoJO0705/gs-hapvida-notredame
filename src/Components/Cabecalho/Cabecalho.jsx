544a4381e892ad4b01fea563367d7ca16fd9edca
ThiagoJO0705 <rm551624@fiap.com.br>
2023-11-22 13:58:05 -0300

    site finalizado

--- src/Components/Cabecalho/Cabecalho.jsx
+++ src/Components/Cabecalho/Cabecalho.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-scroll';
+import { Link as LinkRouter } from 'react-router-dom';
 import { FaUserCircle } from "react-icons/fa";
 import "./Cabecalho.scss"
 import { useEffect, useState } from 'react';
@@ -57,9 +58,72 @@ export default function Cabecalho() {
         sessionStorage.removeItem("data-user") || localStorage.removeItem("data-user")
         window.location.reload()
     }
+    if(sessionStorage.getItem("token-user") || localStorage.getItem("token-user")){
 
-    return(
-        <div className='cabecalho'>
+        return(
+            <div className='cabecalho'>
+                <header>
+                        
+                    <img className='logo' src={logo} alt="Logo HealthTrackr" onClick={logoClick}/>
+    
+                    <nav className='menu'>
+                        <ul>
+                            <li>
+                            <Link to="solucao" smooth={true} duration={0}>Solução</Link>
+                            <Link to="acao" smooth={true} duration={0}>Ação</Link>
+                            <Link to="funcao" smooth={true} duration={0}>Função</Link>
+                            <Link to="vantagem" smooth={true} duration={0}>Vantagens</Link>
+                            <Link to="desafio" smooth={true} duration={0}>Desafios</Link>
+                            </li>
+                        </ul>
+                    </nav>
+    
+    
+                    <div className='titulo'>
+                        <h1>HealthTrackr</h1>
+                    </div>
+    
+                    <label  className="burger" htmlFor="burger">
+                        <input onClick={handleClickMenu} type="checkbox" id="burger"/>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </label>
+    
+    
+                    <nav className={openMenu ? "menu-aparecendo" : "menu-mobile"}>
+                        <ul>
+                            <li>
+                            <Link to="solucao" smooth={true} duration={0}>Solução</Link>
+                            <Link to="acao" smooth={true} duration={0}>Ação</Link>
+                            <Link to="funcao" smooth={true} duration={0}>Função</Link>
+                            <Link to="vantagem" smooth={true} duration={0}>Vantagens</Link>
+                            <Link to="desafio" smooth={true} duration={0}>Desafios</Link>
+                            </li>
+                        </ul>
+                    </nav>
+    
+    
+                    <div className='perfil'>
+                        <div className='info-usuario'>
+                            <div className='nome-email'>
+                                <p className='nome-desk'>{storedData.nome}</p>
+                                <p className='email-desk'>{storedData.email}</p>
+                            </div>
+                            <FaUserCircle cursor={"pointer"} size={60} onClick={handleClick}/>
+                        </div>
+                        <div className={openLogout ? "logout" : "logout-fechado"}>
+                            <p onClick={handleClickLogout} className='btn-logout'>Logout</p>
+                            <p className='nome-logout'>{storedData.nome}</p>
+                            <p className='email-logout'>{storedData.email}</p>
+                        </div>
+                    </div>
+                </header>
+            </div>
+        )
+    } else {
+        return (
+            <div className='cabecalho'>
             <header>
                     
                 <img className='logo' src={logo} alt="Logo HealthTrackr" onClick={logoClick}/>
@@ -101,20 +165,15 @@ export default function Cabecalho() {
                     </ul>
                 </nav>
 
-
-                <div className='perfil'>
-                    <div className='info-usuario'>
-                        <p>{storedData.nome}</p>
-                        <p>{storedData.email}</p>
-                        <FaUserCircle cursor={"pointer"} size={60} onClick={handleClick}/>
-                    </div>
-                    <div className={openLogout ? "logout" : "logout-fechado"}>
-                        <p onClick={handleClickLogout} className='btn-logout'>Logout</p>
-                        <p className='nome-logout'>{storedData.nome}</p>
-                        <p>{storedData.email}</p>
-                    </div>
+                <div className='container-btn'>
+                    <LinkRouter to="/login">
+                        <button className="btn">Login</button>
+                    </LinkRouter>
                 </div>
+                
+
             </header>
         </div>
-    )
+        )
+    }
 }
