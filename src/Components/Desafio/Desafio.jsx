5af9d8c8c6680167b58afdff6ecb267b0736ea39
santos-nikolas <rm551566@fiap.com.br>
2023-11-23 11:49:18 -0300

    criando e estilizando componente desafio

---
+++ src/Components/Desafio/Desafio.jsx
@@ -0,0 +1,38 @@
+import "./Desafio.scss"
+
+
+export default function Desafio () {
+
+
+
+    return(
+        <>
+            <section className="desafio" id="desafio">
+                <div>
+                    <div className="container-titulo">
+                        <h1>Desafios da Saúde na Atualidade</h1>
+                        <p>A busca por uma vida saudável enfrenta diversos desafios, refletindo a necessidade urgente de inovações. Estes obstáculos incluem:</p>
+                    </div>
+
+                    <div className="container-lista">
+                        <ul>
+                            <li><b>Prevenção Ignorada:</b> Abordagens reativas, tratando doenças após o surgimento, em vez de preveni-las.</li>
+                            <li></li><b>Acesso Limitado à Saúde:</b> Restrições geográficas dificultam a detecção precoce e cuidados adequados.
+                            <li></li><b>Mudança de Hábitos Desafiadora:</b> Dificuldade generalizada na adoção de hábitos saudáveis.
+                            <li></li><b>Dados Desconectados:</b> Falta de integração eficiente compromete a compreensão global da saúde.
+                            <li></li><b>Enfoque Doentio:</b> Ênfase tradicional no tratamento em detrimento da promoção ativa da saúde.
+                        </ul>
+                    </div>
+
+                    <div className="container-conclusao">
+                        <p>Enfrentamos esses desafios com nossa solução inovadora, priorizando a prevenção, personalização e o engajamento para uma vida mais saudável.</p>
+                    </div>
+                </div>
+
+            </section>
+        </>
+
+
+
+    )
+}
